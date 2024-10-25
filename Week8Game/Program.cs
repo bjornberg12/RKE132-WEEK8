5ee30bd9ae57aa28a5038499843f48acd2ef40fb
Hugo L <hugo.liivaleht@tktk.ee>
2024-10-25 11:29:45 +0300

    COMMIT

---
+++ Week8Game/Program.cs
@@ -0,0 +1,39 @@
+﻿string filePath = "/Users/hugoliivaleht/projects/data/villains.txt";
+string[] dataFromFile = File.ReadAllLines(filePath);
+foreach (string element in dataFromFile)
+{
+    Console.WriteLine(element);
+}
+
+string folderPath = "/Users/hugoliivaleht/projects/data/heroes.txt";
+string[] dataFromFiles = File.ReadAllLines(folderPath);
+foreach (string element in dataFromFiles)
+{
+    Console.WriteLine(element);
+}
+
+
+
+
+//string[] heroes = { "Harry Potter", "Lara Croft", "Patrick", "Luke Skywalker" };
+//string[] villains = { "Doofenzmirts", "Darth Vader", "Dexter"};
+
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "Lego" };
+string hero = GetRandomValueFromArray(filePath);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+
+
+string villain = GetRandomValueFromArray(folderPath);
+string villainWeapon = GetRandomValueFromArray(weapon);
+
+Console.WriteLine($"Today {villain} with {villainWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
